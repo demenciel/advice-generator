fb3a1b3d7c04f963b4046e487f86781352ad4b6c
Alex Couture <79432799+demenciel@users.noreply.github.com>
2022-08-04 17:19:37 -0400

    Add files via upload
    Front End Mentor Challenge

---
+++ script.js
@@ -0,0 +1,35 @@
+let id = 90
+let adviceID = document.querySelector('#advice-number');
+let adviceText = document.querySelector('.advice');
+let btn = document.querySelector('.dice-img');
+
+function getAdvice() {
+    let url = "https://api.adviceslip.com/advice";
+
+    let request = new XMLHttpRequest();
+
+    request.open('GET', url);
+    request.responseType = 'json';
+    request.send();
+
+    request.onload = function() {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            if(request.status === 200) {
+                let response = request.response
+
+                adviceID.textContent = response.slip.id;
+                adviceText.textContent = "''" + response.slip.advice + "''";
+            } else {
+                alert('ERROR');
+            }
+        }
+    }
+};
+
+getAdvice();
+
+btn.addEventListener('click', ()=> {
+    getAdvice();
+})
+
+
